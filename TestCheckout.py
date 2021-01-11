43deabdb63acaa7665068a7c92eec61b787ef8dd
siryoav <siryoav@gmail.com>
2021-01-11 13:26:13 +0200

    Added test for multiple items

--- TestCheckout.py
+++ TestCheckout.py
@@ -11,3 +11,10 @@ def test_CanCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
