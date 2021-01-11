28561f19757db29f7c878816248e8bb564eb614d
siryoav <siryoav@gmail.com>
2021-01-11 12:10:29 +0200

    Removed unnecessary tests

--- TestCheckout.py
+++ TestCheckout.py
@@ -5,13 +5,7 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
-    return checkout
-
-def test_CanAddItemPrice(checkout):
-    checkout.addItemPrice("a", 1)
-
-def test_CanAddItem(checkout):
-    checkout.addItem("a")
+    return checkouts
 
 def test_CanCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
