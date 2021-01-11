e0970c3cd15f4c9b0a12161fe22e844ff9c7a271
siryoav <siryoav@gmail.com>
2021-01-11 14:31:00 +0200

    Added bad item test

--- TestCheckout.py
+++ TestCheckout.py
@@ -21,10 +21,13 @@ def test_GetCorrectTotalWithMultipleItems(checkout):
 def test_CanAddDiscountRule(checkout):
     checkout.addDiscount("a", 3, 2)
 
-
 def test_CanApplyDiscountRule(checkout):
     checkout.addDiscount("a", 3, 2)
     checkout.addItem("a")
     checkout.addItem("a")
     checkout.addItem("a")
     assert checkout.calculateTotal() == 2
+
+def test_ExceptionWithBadItem(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
