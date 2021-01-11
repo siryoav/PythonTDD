3963abacfa2007ab2e8f657f2fc1e3372ee6a492
siryoav <siryoav@gmail.com>
2021-01-11 13:53:42 +0200

    Refactor

--- TestCheckout.py
+++ TestCheckout.py
@@ -5,16 +5,15 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 def test_CanCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 def test_GetCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3
